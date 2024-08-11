a7fe511f16ae3eb46b7a40bfe9bce232a75a3b72
Shivaji <shivajisingh1608@gmail.com>
2024-08-11 22:04:28 +0530

    changed for errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,24 +42,3 @@ function Header({ totalCount ,user,setUser}) {
 
 export default WithUserHoc(Header);
 
-
-
-
-
-echo "# NextGenKart" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/shivajisingh16/NextGenKart.git
-git push -u origin main
-
-
-
-
-
-
-
-git remote add origin https://github.com/shivajisingh16/NextGenKart.git
-git branch -M main
-git push -u origin main
